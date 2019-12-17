f899faf280595268c1d5b64e101020363e9011f7
jwei98 <justin.lj.wei@gmail.com>
2019-12-17 15:38:58 -0800

    Added launch request handler and is_currently_playing function.

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+"""
+Main driver for Math Dojo Alexa skill.
+"""
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
@@ -8,12 +11,36 @@ sb = SkillBuilder()
 
 
 @sb.request_handler(can_handle_func=is_request_type("LaunchRequest"))
-def launch_request_handler(handler_input):
-    # type: (HandlerInput) -> Response
-    speech_text = "Welcome to the Alexa Skills Kit, you can say hello!"
-    handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Hello World", speech_text)).set_should_end_session(False)
+def launch_request_handler(handler_input: HandlerInput) -> Response:
+    """Handler for skill launch.
+
+    Also handles setting initial game state.
+    """
+    # Set session attributes.
+    # TODO: This could probably be abstracted to a common class.
+    attr = handler_input.attributes_manager.persistent_attributes
+    if not attr:
+        attr['score'] = 0
+        attr['questions_asked'] = 0
+        attr['game_state'] = 'ENDED'
+    handler_input.attributes_manager.session_attributes = attr
+
+    speech_text = ("Welcome to the Math Dojo! Would you like to play? "
+                   "If so, specify whether you'd like to practice Addition, "
+                   " Subtraction, Multiplication, or Division")
+    reprompt = ("Would you like to practice Addition, Subtraction, "
+                "Multiplication, or Division?")
+    handler_input.response_builder.speak(speech_text).ask(reprompt)
     return handler_input.response_builder.response
+
+
+"""Helper Functions."""
+
+
+def is_currently_playing(handler_input: HandlerInput) -> bool:
+    """Determines whether user is in the middle of a game."""
+    session_attr = handler_input.attributes_manager.session_attributes
+    return session_attr.get('game_state') == 'STARTED'
 
 
 handler = sb.lambda_handler()

99056e60a615acb092f09f622ec9d9b936a0a76a
Alex Wei <alexwei@Alexs-MacBook-Pro-2.local>
2019-12-26 19:31:59 -0800

    wrote table_number_intent_handler function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 """
 Main driver for Math Dojo Alexa skill.
+testing
 """
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.utils import is_request_type, is_intent_name
@@ -36,11 +37,38 @@ def launch_request_handler(handler_input: HandlerInput) -> Response:
 
 """Helper Functions."""
 
-
-def is_currently_playing(handler_input: HandlerInput) -> bool:
-    """Determines whether user is in the middle of a game."""
+@sb.request_handler(can_handle_func=lambda input:
+                    not currently_playing(input) and
+                    is_intent_name("TableNumberIntent")(input))
+def table_number_intent_handler(handler_input):
+    """Handler for Yes Intent, only if the player said yes for
+    a new game.
+    """
+    # type: (HandlerInput) -> Response
     session_attr = handler_input.attributes_manager.session_attributes
-    return session_attr.get('game_state') == 'STARTED'
+    session_attr['tableNumber'] = int(handler_input.request_envelope.request.intent.slots[
+                                          "number"].value)
+    session_attr['score'] = 0
+    session_attr['numQuestionsRemaining'] = 10
+    session_attr['lastQuestionAsked'] = (session_attr['tableNumber'],
+                                         random.randint(0, session_attr['tableNumber']))
+    session_attr['gameStarted'] = True;
+
+    speech_text = ("Great! Let's begin. What is "
+                   + session_attr['lastQuestionAsked'][0] + ' '
+                   + session_attr['operator'] + ' '
+                   + session_attr['lastQuestionAsked'][1] + '?'
+                   )
+
+    reprompt = ("What is the solution to "
+                + session_attr['lastQuestionAsked'][0] + ' '
+                + session_attr['operator'] + ' '
+                + session_attr['lastQuestionAsked'][1] + '?'
+                )
+
+    handler_input.response_builder.speak(speech_text).ask(reprompt)
+    return handler_input.response_builder.response
+
 
 
 handler = sb.lambda_handler()

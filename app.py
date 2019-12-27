c9bcd55d217d47168aee7c485a21bea443d416ce
jwei98 <justin.lj.wei@gmail.com>
2019-12-26 19:55:45 -0800

    Initial commit for choose game type handler

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model import Response
 from ask_sdk_model.ui import SimpleCard
 
+gametype_to_operator = {"ADDITION": "+",
+                        "SUBTRACTION": "-",
+                        "MULTIPLICATION": "*",
+                        "DIVISION": "/"}
 
 sb = SkillBuilder()
 
@@ -22,6 +26,25 @@ def launch_request_handler(handler_input: HandlerInput) -> Response:
                    " Subtraction, Multiplication, or Division")
     reprompt = ("Would you like to practice Addition, Subtraction, "
                 "Multiplication, or Division?")
+
+    handler_input.response_builder.speak(speech_text).ask(reprompt)
+    return handler_input.response_builder.response
+
+
+@sb.request_handler(can_handle_func=lambda input:
+                    not currently_playing(input) and
+                    is_intent_name("GameTypeIntent")(input))
+def choose_game_type_handler(handler_input):
+    game_type = (handler_input.request_envelope.request.intent.slots[
+        "GameType"].value)
+    session_attr = handler_input.attributes_manager.session_attributes
+    session_attr['operator'] = gametype_to_operator[game_type]
+
+    speech_text = (f"Okay! You are about to begin your {game_type} training, "
+                   "but first, choose which number table you'd like to
+                   practice.")
+    reprompt = ("Choose which table you'd like to practice by saying a number "
+                "like 3 or 7.")
 
     handler_input.response_builder.speak(speech_text).ask(reprompt)
     return handler_input.response_builder.response
